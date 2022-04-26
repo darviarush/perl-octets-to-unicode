92414efcb2ba6359b4bcf453667cbf69807d27e9
Yaroslav O. Kosmina <darviarush@mail.ru>
2022-04-26 18:07:12 +0300

    master Initial commit.

---
+++ lib/Ru/Perl/Tidy.pm
@@ -0,0 +1,39 @@
+package Ru::Perl::Tidy;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Ru::Perl::Tidy - It's new $module
+
+=head1 SYNOPSIS
+
+    use Ru::Perl::Tidy;
+
+=head1 DESCRIPTION
+
+Ru::Perl::Tidy is ...
+
+=head1 LICENSE
+
+Copyright (C) Yaroslav O. Kosmina.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Yaroslav O. Kosmina E<lt>darviarush@mail.ruE<gt>
+
+=cut
+
